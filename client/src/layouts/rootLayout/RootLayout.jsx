0fb9bd81eb7648c552e699a17e69ab46001590ef
Sauharda <077bct036.suharda@sagarmatha.edu.np>
2024-12-07 15:57:51 +0545

    feat:clerk auth and client auth correction

--- client/src/layouts/rootLayout/RootLayout.jsx
+++ client/src/layouts/rootLayout/RootLayout.jsx
@@ -1,39 +1,33 @@
 import { Link, Outlet } from "react-router-dom";
-import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
+
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 import "./rootLayout.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
 
 const queryClient = new QueryClient();
 
 const RootLayout = () => {
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <QueryClientProvider client={queryClient}>
-        <div className="rootLayout">
-          <header>
-            <Link to="/" className="logo">
-              <img src="/logo.png" alt="" />
-              <span>BankHelp AI</span>
-            </Link>
-            <div className="user">
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </div>
-          </header>
-          <main>
-            <Outlet />
-          </main>
-        </div>
-      </QueryClientProvider>
-    </ClerkProvider>
+    <QueryClientProvider client={queryClient}>
+      <div className="rootLayout">
+        <header>
+          <Link to="/" className="logo">
+            <img src="/logo.png" alt="" />
+            <span>BankHelp AI</span>
+          </Link>
+          <div className="user">
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 };
 

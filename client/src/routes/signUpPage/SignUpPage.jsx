bd55b51f0d98cbd3e4c16289f49507cb5db6aa5d
BinayakPradhan <binayakprdhn@gmail.com>
2024-12-08 18:29:32 +0545

    Fixed Signup

--- client/src/routes/signUpPage/SignUpPage.jsx
+++ client/src/routes/signUpPage/SignUpPage.jsx
@@ -4,7 +4,7 @@ import "./signUpPage.css";
 const SignUpPage = () => {
   return (
     <div className="signUpPage">
-      <SignUp signInUrl="/sign-in" redirectTo="/dashboard" />
+      <SignUp signInUrl="/sign-in" fallbackRedirectUrl="/dashboard" />
     </div>
   );
 };

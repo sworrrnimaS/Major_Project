878a6bf44053d93779d371e369ae660b8395912c
BinayakPradhan <binayakprdhn@gmail.com>
2025-02-07 17:06:14 +0545

    New commit

--- client/src/routes/dashboardPage/DashboardPage.jsx
+++ client/src/routes/dashboardPage/DashboardPage.jsx
@@ -1,59 +1,59 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { MoveUp } from "lucide-react";
 import "./dashboardPage.css";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { DNA } from "react-loader-spinner";
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+// import { useNavigate } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import { DNA } from "react-loader-spinner";
 
 //Yo page ma chai naya chat creation part huncha naya session create garna POST request pathako cha server lai, since paila chai New Chat button thenna, naya chat ko lagi session create garna euta query pathauna parthyo which is done by this page, natra QA pairs is handled by NewPrompt and dekhaune part is ChatPage
 
 const DashboardPage = () => {
-  const queryClient = useQueryClient();
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
+  // const navigate = useNavigate();
+  // const queryClient = useQueryClient();
+  // const [loading, setLoading] = useState(true);
 
   // New chat session banaune part handle garne ho yaha bata
-  const mutation = useMutation({
-    mutationFn: async () => {
-      const response = await fetch(
-        `http://localhost:3000/session/createSession`
-      );
-      if (!response.ok) {
-        throw new Error("Failed to create session");
-      }
-      return response.json();
-    },
+  // const mutation = useMutation({
+  //   mutationFn: async () => {
+  //     const response = await fetch(
+  //       `http://localhost:3000/session/createSession`
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error("Failed to create session");
+  //     }
+  //     return response.json();
+  //   },
 
-    onSuccess: (data) => {
-      const sessionId = data.session._id;
-      queryClient.invalidateQueries({ queryKey: ["userSessions"] }); // Update all session queries
-      navigate(`/dashboard/chats/${sessionId}`); //Navigate to ChatPage
-    },
+  //   onSuccess: (data) => {
+  //     const sessionId = data.session._id;
+  //     queryClient.invalidateQueries({ queryKey: ["userSessions"] }); // Update all session queries
+  //     navigate(`/dashboard/chats/${sessionId}`); //Navigate to ChatPage
+  //   },
 
-    onError: (error) => {
-      console.error("Error creating session:", error);
-      setLoading(false); // Set loading to false if an error occurs
-    },
-  });
+  //   onError: (error) => {
+  //     console.error("Error creating session:", error);
+  //     setLoading(false); // Set loading to false if an error occurs
+  //   },
+  // });
 
-  useEffect(() => {
-    // Create a session when component mounts
-    mutation.mutate();
-  }, []);
+  // useEffect(() => {
+  //   // Create a session when component mounts
+  //   mutation.mutate();
+  // }, []);
 
-  if (loading) {
-    return (
-      <DNA
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="dna-loading"
-        wrapperStyle={{}}
-        wrapperClass="dna-wrapper"
-      />
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <DNA
+  //       visible={true}
+  //       height="80"
+  //       width="80"
+  //       ariaLabel="dna-loading"
+  //       wrapperStyle={{}}
+  //       wrapperClass="dna-wrapper"
+  //     />
+  //   );
+  // }
   return (
     <div className="dashboardPage">
       <div className="texts">
